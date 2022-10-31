1e22d6065f6f84194a86438e7e466527025a3bc1
Andrei.Nica <andreinica85@yahoo.com>
2022-10-31 12:24:03 +0100

    Project init

---
+++ src/main/java/com/flyingticketsapp/classexercise/ClassexerciseApplication.java
@@ -0,0 +1,13 @@
+package com.flyingticketsapp.classexercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassexerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassexerciseApplication.class, args);
+	}
+
+}
