c20dc1fdf6762c0957523523b478ecac4879e52c
Piotr Kubacki <piotr.kubacki@solera.com>
2022-10-31 16:00:23 +0100

    first test

--- src/test/java/com/flyingticketsapp/classexercise/ClassexerciseApplicationTests.java
+++ src/test/java/com/flyingticketsapp/classexercise/ClassexerciseApplicationTests.java
@@ -2,12 +2,28 @@ package com.flyingticketsapp.classexercise;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootTest
 class ClassexerciseApplicationTests {
+    @Test
+    void contextLoads() {
 
-	@Test
-	void contextLoads() {
-	}
+    }
 
+    public boolean bookASeat(String seat, List<String> seats) {
+        return seats.remove(seat);
+    }
+
+    @Test
+    public void checkFlightDate() {
+        LocalDate flightDate = LocalDate.now().plusDays(5);
+        LocalDate currentDate = LocalDate.now();
+
+        Assert.isTrue(flightDate.compareTo(currentDate) >= 0, "flightDate should not be in past date");
+    }
 }
