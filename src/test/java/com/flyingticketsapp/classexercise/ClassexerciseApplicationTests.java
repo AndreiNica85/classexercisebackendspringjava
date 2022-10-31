82d7a22275587b5e9b3919ffc7e8a0ee5b77932d
Rodrigo Gamero <rodrigo.gamero.luque@gmail.com>
2022-10-31 16:41:14 +0100

    Changes

--- src/test/java/com/flyingticketsapp/classexercise/ClassexerciseApplicationTests.java
+++ src/test/java/com/flyingticketsapp/classexercise/ClassexerciseApplicationTests.java
@@ -9,14 +9,6 @@ import org.springframework.util.Assert;
 @SpringBootTest
 class ClassexerciseApplicationTests {
 
-	@ParameterizedTest()
-	@ValueSource(ints = {98, 100,101,200})
-	void maxNumberSits(int planeSits) {
-		int maxNumberOfSits = 100;  // Maximum sits per plane is 100;
-		Assert.isTrue(planeSits < maxNumberOfSits, "Maximum number of sits in a plane is 100" );
-	}
-
-	//this is a test comment
 
 
 }
