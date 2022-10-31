9e0eca0ca900548ca0961eef080dbca1e5120f28
Rodrigo Gamero <rodrigo.gamero.luque@gmail.com>
2022-10-31 15:49:43 +0100

    "Done - Testing lugagge less than 10"

--- src/test/java/com/flyingticketsapp/classexercise/ClassexerciseApplicationTests.java
+++ src/test/java/com/flyingticketsapp/classexercise/ClassexerciseApplicationTests.java
@@ -1,13 +1,25 @@
 package com.flyingticketsapp.classexercise;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.event.annotation.BeforeTestMethod;
+import org.springframework.util.Assert;
 
 @SpringBootTest
 class ClassexerciseApplicationTests {
 
+
 	@Test
 	void contextLoads() {
+	}
+
+	@ParameterizedTest
+	@ValueSource(doubles = {10.5, 9, 0, 1, 11})
+	public void lugaggeLessThanTen(double maxLugagge){
+		Assert.isTrue(maxLugagge <= 10, "Testing lugagge");
+
 	}
 
 }
