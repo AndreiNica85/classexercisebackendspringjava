fd2e7ecc5ec75be3c6e95d1ce90c3d08ac4ac11d
Piotr Kubacki <piotr.kubacki@solera.com>
2022-10-31 16:05:03 +0100

    first test

--- src/test/java/com/flyingticketsapp/classexercise/ClassexerciseApplicationTests.java
+++ src/test/java/com/flyingticketsapp/classexercise/ClassexerciseApplicationTests.java
@@ -5,7 +5,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.util.Assert;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.List;
 
 @SpringBootTest
