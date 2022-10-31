4a5b798f2f760cc1624e93a9034c6f3323a738a1
Rodrigo Gamero <rodrigo.gamero.luque@gmail.com>
2022-10-31 16:31:24 +0100

    all test

--- src/test/java/com/flyingticketsapp/classexercise/ClassexerciseApplicationTests.java
+++ src/test/java/com/flyingticketsapp/classexercise/ClassexerciseApplicationTests.java
@@ -1,13 +1,21 @@
 package com.flyingticketsapp.classexercise;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
 
 @SpringBootTest
 class ClassexerciseApplicationTests {
 
-	@Test
-	void contextLoads() {
+	@ParameterizedTest()
+	@ValueSource(ints = {98, 100,101,200})
+	void maxNumberSits(int planeSits) {
+		int maxNumberOfSits = 100;  // Maximum sits per plane is 100;
+		Assert.isTrue(planeSits < maxNumberOfSits, "Maximum number of sits in a plane is 100" );
 	}
+
+
 
 }
