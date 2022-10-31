11bb483bb45ba7ad7a01a60d7c4d83c82ccaa559
Piotr Kubacki <piotr.kubacki@solera.com>
2022-10-31 16:45:55 +0100

    first test

--- src/test/java/com/flyingticketsapp/classexercise/BookSeatsTest.java
+++ src/test/java/com/flyingticketsapp/classexercise/BookSeatsTest.java
@@ -14,5 +14,38 @@ import java.util.List;
 
 public class BookSeatsTest {
 
+    public boolean bookASeat(String seat, List<String> seats) {
+        return seats.remove(seat);
+    }
 
+    @ParameterizedTest
+    @ValueSource(strings = {"15B","16B","17B","18B"})
+    public void testIfSeatIsAvailableForBooking(String seatAvailable) {
+
+        List<String> seats = new ArrayList<>();
+        seats.add("15B");
+        seats.add("16B");
+        seats.add("17B");
+        seats.add("18B");
+        Assert.isTrue(bookASeat(seatAvailable, seats), "Seat should be available for booking");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"000","001","002","003","004","005","006","007","008"})
+    public void testIfSeatIsNotAvailableForBooking(String seatNotAvailable) {
+
+        List<String> seats = new ArrayList<>();
+        seats.add("15B");
+        seats.add("16B");
+        seats.add("17B");
+        seats.add("18B");
+        Assert.isTrue(!bookASeat(seatNotAvailable, seats), "Seat shouldnot be available for booking");
+    }
+
+    @Test
+    public void checkFlightDate() {
+        LocalDate flightDate = LocalDate.now().plusDays(5);
+        LocalDate currentDate = LocalDate.now();
+        Assert.isTrue(flightDate.compareTo(currentDate) >= 0, "flightDate should not be in past date");
+    }
 }
