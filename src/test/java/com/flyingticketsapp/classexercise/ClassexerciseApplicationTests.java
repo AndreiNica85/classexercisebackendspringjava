e6a35635472bb6430867285f3523308687509cf5
Piotr Kubacki <piotr.kubacki@solera.com>
2022-10-31 16:44:20 +0100

    first test

--- src/test/java/com/flyingticketsapp/classexercise/ClassexerciseApplicationTests.java
+++ src/test/java/com/flyingticketsapp/classexercise/ClassexerciseApplicationTests.java
@@ -1,6 +1,8 @@
 package com.flyingticketsapp.classexercise;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.util.Assert;
 
@@ -17,6 +19,31 @@ class ClassexerciseApplicationTests {
 
     public boolean bookASeat(String seat, List<String> seats) {
         return seats.remove(seat);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"15B","16B","17B","18B"})
+    public void testIfSeatIsAvailableForBooking(String seatAvailable) {
+
+        List<String> seats = new ArrayList<>();
+        seats.add("15B");
+        seats.add("16B");
+        seats.add("17B");
+        seats.add("18B");
+        Assert.isTrue(bookASeat(seatAvailable, seats), "Seat should be available for booking");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"000","001","002","003","004","005","006","007","008"})
+    public void testIfSeatIsNotAvailableForBooking(String seatNotAvailable) {
+
+        List<String> seats = new ArrayList<>();
+        seats.add("15B");
+        seats.add("16B");
+        seats.add("17B");
+        seats.add("18B");
+
+        Assert.isTrue(!bookASeat(seatNotAvailable, seats), "Seat shouldnot be available for booking");
     }
 
     @Test
