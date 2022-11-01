5836aef99c371acb7077a889c604e3ac374baa65
Andrei.Nica <andreinica85@yahoo.com>
2022-11-01 14:55:26 +0100

    Commit model classes

--- src/main/java/com/flyingticketsapp/classexercise/service/FlightDAOService.java
+++ src/main/java/com/flyingticketsapp/classexercise/service/FlightDAOService.java
@@ -1,6 +1,15 @@
 package com.flyingticketsapp.classexercise.service;
 
+import com.flyingticketsapp.classexercise.model.Flight;
+import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
 public class FlightDAOService {
-        
+
+    private static List<Flight> flights = new ArrayList<>();
+    
+
 }
