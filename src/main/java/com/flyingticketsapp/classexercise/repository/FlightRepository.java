d96e39147484f7b8fafde2720f2d5f35661bed2c
Piotr Kubacki <piotr.kubacki@solera.com>
2022-11-01 21:38:01 +0100

    fixed repository types

--- src/main/java/com/flyingticketsapp/classexercise/repository/FlightRepository.java
+++ src/main/java/com/flyingticketsapp/classexercise/repository/FlightRepository.java
@@ -1,8 +1,9 @@
 package com.flyingticketsapp.classexercise.repository;
 
+import com.flyingticketsapp.classexercise.model.Flight;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface FlightRepository extends JpaRepository {
+public interface FlightRepository extends JpaRepository<Flight, Integer> {
 }
--- src/main/resources/data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO traveller (id, age, email, forename, nationality, pseudo_password, surname)
-VALUES  (1,20, 'random@email.com', 'Piotr', 'Polish', 'randomhash', 'Kubacki'),
-	(2,20, 'random@email.com', 'Piotr', 'Polish', 'randomhash', 'Kubacki'),
-	(3,20, 'random@email.com', 'Piotr', 'Polish', 'randomhash', 'Kubacki');
