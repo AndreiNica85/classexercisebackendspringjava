3903b8e08bad362962bff83ac295212c4dff5521
Andrei.Nica <andreinica85@yahoo.com>
2022-11-03 14:14:33 +0100

    Commited Removed Plane

--- src/main/java/com/flyingticketsapp/classexercise/model/Flight.java
+++ src/main/java/com/flyingticketsapp/classexercise/model/Flight.java
@@ -1,12 +1,11 @@
 package com.flyingticketsapp.classexercise.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.format.annotation.DateTimeFormat;
+
 
 import java.time.LocalDate;
 import java.time.LocalTime;
@@ -29,9 +28,6 @@ public class Flight {
     private String airline;
     private boolean isRoundTrip;
     private double price;
-
-    @OneToOne
-    private Plane plane;
 
     @ManyToOne
     private Traveller traveller;
