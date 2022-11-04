031393ee4df325318167210fb231589f30dc9e8c
Piotr Kubacki <piotr.kubacki@solera.com>
2022-11-04 16:14:16 +0100

    analytics feature fixed and refactored

--- src/main/java/com/flyingticketsapp/classexercise/service/AnalyticsService.java
+++ src/main/java/com/flyingticketsapp/classexercise/service/AnalyticsService.java
@@ -16,12 +16,12 @@ public class AnalyticsService {
 
     public Map<String,Long> getAllInformation(){
         Map<String,Long> map = new HashMap<>();
-        map.put("countUnfinishedNotPaidFlights", Analytics.countUnfinishedNotPaidFlights);
-        map.put("countHomePageVisits", Analytics.countHomePageVisits);
-        map.put("countOriginSelected", Analytics.countOriginSelected);
-        map.put("countDestinationsSelected", Analytics.countDestinationsSelected);
-        map.put("countPaidFlights", Analytics.countPaidFlights);
-        map.put("countAllPagesVisitsTraveller", Analytics.countAllPagesVisitsTraveller + Analytics.countAllPagesVisitsFlight);
+        map.put("countUnfinishedNotPaidFlights", analytics.countUnfinishedNotPaidFlights);
+        map.put("countHomePageVisits", analytics.countHomePageVisits);
+        map.put("countOriginSelected", analytics.countOriginSelected);
+        map.put("countDestinationsSelected", analytics.countDestinationsSelected);
+        map.put("countPaidFlights", analytics.countPaidFlights);
+        map.put("countAllPagesVisitsTraveller", analytics.countAllPagesVisitsTraveller + analytics.countAllPagesVisitsFlight);
         return map;
     }
 }
